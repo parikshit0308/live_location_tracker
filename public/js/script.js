0631e11bfd1147c54ed25ce5dab8dd7e1be35433
parikshit0308 <sparikshit955@gmail.com>
2024-07-08 16:37:20 +0530

    update

--- public/js/script.js
+++ public/js/script.js
@@ -16,7 +16,7 @@ if (navigator.geolocation){
 )
 }
 
-const map = L.map("map").setView([0,0],10);
+const map = L.map("map").setView([0,0],15);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
     attribution: "Mera_Map"
