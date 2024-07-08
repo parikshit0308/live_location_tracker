f822a68007ef0b513bb716d36c05321e75302eba
parikshit0308 <sparikshit955@gmail.com>
2024-07-08 16:52:53 +0530

    update

--- public/js/script.js
+++ public/js/script.js
@@ -26,7 +26,7 @@ const markers =  {};
 
 socket.on('recieve-location', (data) => {
     const{id, latitude, longitude} = data;
-    map.setView([latitude, longitude]);
+    map.setView([latitude, longitude],20);
     if(markers[id]){
         markers[id].setLatLng([latitude, longitude]);
     } else {
